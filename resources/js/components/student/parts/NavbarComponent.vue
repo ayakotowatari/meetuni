2a634595ca4c450b070f1fa5cd7ba88c70e4131b
Ayako Towatari <kyotayako@gmail.com>
2020-09-09 00:56:16 +0900

    Arranging navbar

--- resources/js/components/student/parts/NavbarComponent.vue
+++ resources/js/components/student/parts/NavbarComponent.vue
@@ -8,12 +8,35 @@
         <span class="font-weight-bold primary--text">meet<span class="font-weight-light">Uni</span></span> 
       </v-toolbar-title>
       <v-spacer></v-spacer>
+       <v-menu open-on-hover offset-y>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          color="primary"
+          dark
+          v-bind="attrs"
+          v-on="on"
+        >
+          Dropdown
+        </v-btn>
+      </template>
+
+      <v-list>
+        <v-list-item
+          v-for="link in links"
+          :key="link.text"
+          router 
+          :to="{ name:link.route, params:{id: id}}"
+        >
+          <v-list-item-title>{{ link.text }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
 
       <v-btn text color="grey" @click="logout">
-            <span>Log out</span>
-            <form id="logout-form" action="/logout" method="POST">
-              <input type="hidden" name="_token" :value="token">
-            </form>
+           <span>Log out</span>
+                <form id="logout-form" action="/logout" method="POST">
+                  <input type="hidden" name="_token" :value="token">
+                </form>
             <v-icon right>mdi-logout-variant</v-icon>
       </v-btn>
     </v-app-bar>
@@ -26,12 +49,11 @@ import { mapState } from 'vuex'
 
   export default {
     data: () => ({
-      drawer: true,
       links: [
-        { icon: 'mdi-home-outline', text: 'My Projects', route: '/inst/events' },
-        { icon: 'mdi-pencil-outline', text: 'Create Events', route: '/inst/create-events' },
-        { icon: 'mdi-file-document-outline', text: 'Drafts', route: '/inst/drafts'},
-        { icon: 'mdi-account-details-outline', text: 'My Profile', route: '/inst/profile' },
+        { icon: 'mdi-calendar-month-outline', text: 'Booked Events', route: '/student/booked-events' },
+        { icon: 'mdi-eye-outline', text: 'Following', route: '/student/following' },
+        { icon: 'mdi-heart-outline', text: 'Liked', route: '/student/liked'},
+        { icon: 'mdi-account-details-outline', text: 'My Profile', route: '/student/profile' },
        ],
     }),
     mounted() {
