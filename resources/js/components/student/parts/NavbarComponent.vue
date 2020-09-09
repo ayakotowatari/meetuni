ce263857176732a109376b373d50f07f4fc1af12
Ayako Towatari <kyotayako@gmail.com>
2020-09-09 11:32:38 +0900

    Arranged Navbar Menu

--- resources/js/components/student/parts/NavbarComponent.vue
+++ resources/js/components/student/parts/NavbarComponent.vue
@@ -8,37 +8,48 @@
         <span class="font-weight-bold primary--text">meet<span class="font-weight-light">Uni</span></span> 
       </v-toolbar-title>
       <v-spacer></v-spacer>
-       <v-menu open-on-hover offset-y>
-      <template v-slot:activator="{ on, attrs }">
-        <v-btn
-          color="primary"
-          dark
-          v-bind="attrs"
-          v-on="on"
-        >
-          Dropdown
-        </v-btn>
-      </template>
+      <v-menu open-on-hover top offset-y>
+          <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                color="primary"
+                text
+                v-bind="attrs"
+                v-on="on"
+              >
+                <span class="mr-2">menu</span>  
+                <v-icon>mdi-account-outline</v-icon>
+              </v-btn>
+          </template>
 
-      <v-list>
-        <v-list-item
-          v-for="link in links"
-          :key="link.text"
-          router 
-          :to="{ name:link.route, params:{id: id}}"
-        >
-          <v-list-item-title>{{ link.text }}</v-list-item-title>
-        </v-list-item>
-      </v-list>
+          <v-list>
+              <v-list-item
+                v-for="link in links"
+                :key="link.text"
+                :to="{ name:link.route, params:{id: id}}"
+              >
+                  <v-list-item-action>
+                      <v-icon left class="grey--text text--darken-3">{{ link.icon }}</v-icon>
+                  </v-list-item-action>
+                  <v-list-item-content>
+                      <v-list-item-title class="grey--text text--darken-3">{{ link.text }}</v-list-item-title>
+                  </v-list-item-content>
+              </v-list-item>
+              <v-divider></v-divider>
+              <v-list-item text @click="logout">
+                  <v-list-item-action>
+                      <v-icon left class="grey--text">mdi-logout-variant</v-icon>
+                  </v-list-item-action>
+                  <v-list-item-content>
+                  <v-list-item-title class="grey--text">Log out</v-list-item-title>
+                      <form id="logout-form" action="/logout" method="POST">
+                        <input type="hidden" name="_token" :value="token">
+                      </form>
+                  </v-list-item-content>
+              </v-list-item>
+          </v-list>
     </v-menu>
 
-      <v-btn text color="grey" @click="logout">
-           <span>Log out</span>
-                <form id="logout-form" action="/logout" method="POST">
-                  <input type="hidden" name="_token" :value="token">
-                </form>
-            <v-icon right>mdi-logout-variant</v-icon>
-      </v-btn>
+      
     </v-app-bar>
 
   </nav>
@@ -50,10 +61,10 @@ import { mapState } from 'vuex'
   export default {
     data: () => ({
       links: [
-        { icon: 'mdi-calendar-month-outline', text: 'Booked Events', route: '/student/booked-events' },
-        { icon: 'mdi-eye-outline', text: 'Following', route: '/student/following' },
-        { icon: 'mdi-heart-outline', text: 'Liked', route: '/student/liked'},
-        { icon: 'mdi-account-details-outline', text: 'My Profile', route: '/student/profile' },
+        { icon: 'mdi-calendar-month-outline', text: 'Booked Events', route: 'booked-events' },
+        { icon: 'mdi-eye-outline', text: 'Following', route: 'following' },
+        { icon: 'mdi-heart-outline', text: 'Liked', route: 'liked'},
+        { icon: 'mdi-account-details-outline', text: 'My Profile', route: 'profile' },
        ],
     }),
     mounted() {
