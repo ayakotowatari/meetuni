161ee0860f173d07fbcfb409cb89c990e29d81eb
Ayako Towatari <kyotayako@gmail.com>
2020-10-04 00:32:55 +0900

    Seeder

--- database/seeds/statusesTableSeeder.php
+++ database/seeds/statusesTableSeeder.php
@@ -13,25 +13,55 @@ class StatusesTableSeeder extends Seeder
     {
         $data = [
             [
+                'status' => 'Ongoing',
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            [
+                'status' => 'Scheduled',
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            [
+                'status' => NULL,
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            [
+                'status' => NULL,
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            [
+                'status' => 'Draft',
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            [
+                'status' => NULL,
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            [
+                'status' => NULL,
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            [
+                'status' => NULL,
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            [
+                'status' => 'Complete',
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            [
                 'status' => 'Sent',
                 "created_at" => now(),
                 "updated_at" => now(),
             ],
-            // [
-            //     'status' => NULL,
-            //     "created_at" => now(),
-            //     "updated_at" => now(),
-            // ],
-            // [
-            //     'status' => NULL,
-            //     "created_at" => now(),
-            //     "updated_at" => now(),
-            // ],
-            // [
-            //     'status' => 'Complete',
-            //     "created_at" => now(),
-            //     "updated_at" => now(),
-            // ],
         ];
                 DB::table('statuses')->insert($data);
     }
