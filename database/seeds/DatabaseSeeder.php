ce0c0a68d09a843132ce283b4ee5f2409b6f8d3f
Ayako Towatari <kyotayako@gmail.com>
2020-10-04 22:15:23 +0900

    Seeder added.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,17 +12,17 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UsersTableSeeder::class);
-        $this->call(CapacitiesTableSeeder::class);
-        $this->call(CategoriesTableSeeder::class);
-        $this->call(CountriesTableSeeder::class);
-        $this->call(InstsTableSeeder::class);
-        $this->call(InstTypesTableSeeder::class);
-        $this->call(LevelsTableSeeder::class);
-        $this->call(RegionsTableSeeder::class);
-        $this->call(StatusesTableSeeder::class);
-        $this->call(SubjectsTableSeeder::class);
-        $this->call(UserTypesTableSeeder::class);
-        $this->call(YearsTableSeeder::class);
+        // $this->call(CapacitiesTableSeeder::class);
+        // $this->call(CategoriesTableSeeder::class);
+        // $this->call(CountriesTableSeeder::class);
+        // $this->call(InstsTableSeeder::class);
+        // $this->call(InstTypesTableSeeder::class);
+        // $this->call(LevelsTableSeeder::class);
+        // $this->call(RegionsTableSeeder::class);
+        // $this->call(StatusesTableSeeder::class);
+        // $this->call(SubjectsTableSeeder::class);
+        // $this->call(UserTypesTableSeeder::class);
+        // $this->call(YearsTableSeeder::class);
 
     }
 }
